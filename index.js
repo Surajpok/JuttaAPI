257b0b494bf8c53b860b14c6c5cc51d3db5a4509
Suraj Pokhrel <Pokhrel004@gmail.com>
2022-05-25 17:35:46 +0545

    db connected

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const express = require("express");
+const app = express();
 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+
+dotenv.config({path:'./config/config.env'});
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => console.log("DBConnection Succesful"))
+.catch((err) => {
+    console.log(err);
+});
+
+
+
+
+app.listen( process.env.PORT || 3000, () =>{
+    console.log("backend server running");
+})
