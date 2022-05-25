a8bf57224745e88820da7f026e4b144a496ee8bc
Suraj Pokhrel <Pokhrel004@gmail.com>
2022-05-25 22:53:18 +0545

    auth

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
 
 
 
@@ -18,6 +20,7 @@ mongoose
 app.use(express.json());
 
 app.use("/api/users",userRoute);
+app.use("/api/auth",authRoute);
 
 
 app.listen( process.env.PORT || 3000, () =>{
--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema(
         password:{type: String, required: true},
         isAdmin:{
             type: Boolean,
-            default: False
+            default: false
         },
     },
     { timestamp: true}
